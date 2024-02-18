57de36bc23447e718d341c8c89bb67369e517aea
SilitraG <66957267+SilitraG@users.noreply.github.com>
2024-02-18 23:39:49 +0200

    JWT Service
    Basic methods of generating and extracting token data

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -1,8 +1,74 @@
 package com.example.demo.service;
 
+import com.example.demo.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
-    
+    private final String SECRET_KEY = "1886144DCE536EBEEA2C150985CED3B2E0B1ADD9347B3C8CBF6A88545200A868";
+
+    public boolean isValid(String token, UserDetails user)
+    {
+        String username = extractUsername(token);
+        return username.equals(user.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token)
+    {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token)
+    {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractUsername(String token)
+    {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver)
+    {
+        Claims claims = extractallClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractallClaims(String token)
+    {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(User user)
+    {
+
+        return Jwts
+                .builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 26*60*60*1000) )
+                .signWith(getSigninKey())
+                .compact();
+    }
+
+    private SecretKey getSigninKey()
+    {
+        byte[] keyByte = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
 }
